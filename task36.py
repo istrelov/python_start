a2baf9757595d196020885e59291773fc4643f7c
AleksandrIstrelov <istrelov004@gmail.com>
2023-04-14 09:43:20 +0300

    task36 clear

--- task36.py
+++ task36.py
@@ -4,15 +4,6 @@
 #как, например, у операции умножения
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    
-    #new_list = list(map(print_one, range(1, num_rows+1), operation, num_columns))
-    #print(new_list)
-    #print(operation())
-    #new_list_2 = list(map(lambda y: y, range(1, num_rows+1)))
-    #print(new_list_2)
-
-    #print(operation(2, 3))
-    #operation(new_list, new_list_2)
    for i in range(num_rows):
         for j in range(num_columns):
             print(operation(i+1, j+1), end = " ")  
